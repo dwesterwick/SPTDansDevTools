882001b591dd0743a7942fb8431d7600c7a47614
Dan Westerwick <dwesterwick@yahoo.com>
2025-11-08 15:07:01 -0500

    Create 7zip archive in post build script

--- bepinex_dev/Shared/PostBuildScript.ps1
+++ bepinex_dev/Shared/PostBuildScript.ps1
@@ -3,8 +3,45 @@
     [string]$modVersion
 )
 
-Write-Host ('Packaging {0} v{1}' -f $modName, $modVersion)
+Write-Host ('Packaging {0} v{1}...' -f $modName, $modVersion)
 
-# Make sure our CWD is where the script lives
 Set-Location $PSScriptRoot
 
+# Create the build folder
+$packageFolderAbsolute = Join-Path $PSScriptRoot "..\dist"
+
+New-Item -ItemType Directory -Path $packageFolderAbsolute -Force | Out-Null
+Remove-Item -Path ('{0}\*' -f $packageFolderAbsolute) -Recurse -Force | Out-Null
+
+# Create server and client folders
+$serverFolderAbsolute = Join-Path $packageFolderAbsolute ('user\mods\{0}' -f $modName)
+$clientFolderAbsolute = Join-Path $packageFolderAbsolute ('BepInEx\plugins\{0}' -f $modName)
+
+New-Item -ItemType Directory -Path $serverFolderAbsolute -Force | Out-Null
+New-Item -ItemType Directory -Path $clientFolderAbsolute -Force | Out-Null
+
+# Copy all files
+
+Write-Host ('Packaging {0} v{1}...copying files...' -f $modName, $modVersion)
+
+$configFileAbsolute = Join-Path $PSScriptRoot 'Config\config.json'
+$serverLibraryAbsolute = Join-Path $PSScriptRoot ('..\Server\bin\Debug\{0}-Server\{0}-Server.dll' -f $modName)
+$clientLibraryAbsolute = Join-Path $PSScriptRoot ('..\Client\bin\Debug\netstandard2.1\{0}-Client.dll' -f $modName)
+
+Copy-Item -Path $configFileAbsolute -Destination $serverFolderAbsolute | Out-Null
+Copy-Item -Path $serverLibraryAbsolute -Destination $serverFolderAbsolute | Out-Null
+
+Copy-Item -Path $clientLibraryAbsolute -Destination $clientFolderAbsolute | Out-Null
+
+# Create 7zip archive
+
+Write-Host ('Packaging {0} v{1}...creating archive...' -f $modName, $modVersion)
+
+$pathTo7z = "C:\Program Files\7-Zip\7z.exe"
+$archiveName = Join-Path $packageFolderAbsolute ('{0}-{1}.7z' -f $modName, $modVersion)
+$sourceFiles = Join-Path $packageFolderAbsolute '*'
+$arguments = "a", "-t7z", $archiveName, $sourceFiles
+
+& $pathTo7z $arguments | Out-Null
+
+Write-Host ('Packaging {0} v{1}...done.' -f $modName, $modVersion)
--- bepinex_dev/Shared/PreBuildScript.ps1
+++ bepinex_dev/Shared/PreBuildScript.ps1
@@ -9,10 +9,13 @@ $modInfoAbsolutePath = Join-Path $PSScriptRoot $modInfoRelativePath
 
 Write-Host ('Updating {0}...' -f $modInfoAbsolutePath)
 
+# Read the original file contents
 $originalContent = Get-Content -Path $modInfoAbsolutePath -Raw
 
+# Update property values
 $updatedContent = $originalContent -replace 'MODVERSION = ".*"' , ('MODVERSION = "{0}"' -f $modVersion)
 
+# Write modified contents back to the file
 $updatedContent | Out-File -FilePath $modInfoAbsolutePath
 
 Write-Host ('Updating {0}...done.' -f $modInfoAbsolutePath)
