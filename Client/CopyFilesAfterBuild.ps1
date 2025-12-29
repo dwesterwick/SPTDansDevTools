2911b78fc342e5445889d65ff421655316429d3e
Dan Westerwick <dwesterwick@yahoo.com>
2025-12-29 11:17:55 -0500

    Removed old files, consolidated file structure, run tests automatically after building

---
+++ Client/CopyFilesAfterBuild.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [string]$modName
+)
+
+Write-Host ('Copying client files for {0}...' -f $modName)
+
+Set-Location $PSScriptRoot
+
+$destinationAbsolute = Join-Path $PSScriptRoot ('..\..\..\BepInEx\plugins\{0}\' -f $modName)
+
+$clientLibraryAbsolute = Join-Path $PSScriptRoot ('bin\Debug\netstandard2.1\{0}-Client.dll' -f $modName)
+
+Copy-Item -Path $clientLibraryAbsolute -Destination $destinationAbsolute | Out-Null
+
+Write-Host ('Copying client files for {0}...done.' -f $modName)
