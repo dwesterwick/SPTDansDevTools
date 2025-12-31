7ffef4d52798209b9bf7371fc06c799e33aa1d73
Dan Westerwick <dwesterwick@yahoo.com>
2025-12-31 01:01:58 -0500

    Fixed pre-build script adding many new lines in ModInfo.cs

--- Shared/UpdateModInfoBeforeBuild.ps1
+++ Shared/UpdateModInfoBeforeBuild.ps1
@@ -19,11 +19,11 @@ $updatedContent = $originalContent
 
 # Update property values
 Write-Host ('Updating {0}...setting properties: GUID={1}, MODNAME={2}, AUTHOR={3}, MODVERSION={4}, SPTVERSIONCOMPATIBILITY={5}.' -f $modInfoAbsolutePath, $guid, $modName, $author, $modVersion, $sptVersion)
-$updatedContent = $updatedContent -replace 'GUID = ".*"' , ('GUID = "{0}"' -f $guid)
-$updatedContent = $updatedContent -replace 'MODNAME = ".*"' , ('MODNAME = "{0}"' -f $modName)
-$updatedContent = $updatedContent -replace 'AUTHOR = ".*"' , ('AUTHOR = "{0}"' -f $author)
-$updatedContent = $updatedContent -replace 'MODVERSION = ".*"' , ('MODVERSION = "{0}"' -f $modVersion)
-$updatedContent = $updatedContent -replace 'SPTVERSIONCOMPATIBILITY = ".*"' , ('SPTVERSIONCOMPATIBILITY = "{0}"' -f $sptVersion)
+$updatedContent = $updatedContent.Trim() -replace 'GUID = ".*"' , ('GUID = "{0}"' -f $guid)
+$updatedContent = $updatedContent.Trim() -replace 'MODNAME = ".*"' , ('MODNAME = "{0}"' -f $modName)
+$updatedContent = $updatedContent.Trim() -replace 'AUTHOR = ".*"' , ('AUTHOR = "{0}"' -f $author)
+$updatedContent = $updatedContent.Trim() -replace 'MODVERSION = ".*"' , ('MODVERSION = "{0}"' -f $modVersion)
+$updatedContent = $updatedContent.Trim() -replace 'SPTVERSIONCOMPATIBILITY = ".*"' , ('SPTVERSIONCOMPATIBILITY = "{0}"' -f $sptVersion)
 
 # Write modified contents back to the file
 $updatedContent | Out-File -FilePath $modInfoAbsolutePath
